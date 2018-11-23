4619eb8b39cb2a20056e2f32133aa1c0d40c3517
korobchinskiy <45297659+korobchinskiy@users.noreply.github.com>
2018-11-24 02:26:31 +0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>
+          Тест PHP
+      </title>
+   </head>
+   <body>
+      <header>
+      
+           Тестируем....
+         
+      </header>
+
+      <?php echo '<p>Привет</p>'; ?>
+
+   </body>
+</html>
